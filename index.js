355b0669c1edbbbe77f8a340fbe4f52d911f8fd0
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-02 22:52:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const logDriverNames = function (drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers){
+  
+}
+
+function driversByRevenue(){
+
+}
+
+function driversByName(){
+
+}
+
+function totalRevenue(){
+
+}
+
+function averageRevenue(){
+
+}
